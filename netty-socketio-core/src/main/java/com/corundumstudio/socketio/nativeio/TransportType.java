69e22f3423434c66a8c1ef4c1206bbe9d51cd6fc
sanjomo <git@santhoshmohan.com>
2025-11-18 15:56:14 +0530

    Update TransportType.java

--- netty-socketio-core/src/main/java/com/corundumstudio/socketio/nativeio/TransportType.java
+++ netty-socketio-core/src/main/java/com/corundumstudio/socketio/nativeio/TransportType.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Project Socketio4J
+ * Copyright (c) 2025 The Socketio4J Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
