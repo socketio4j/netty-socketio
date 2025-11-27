c0272face9092b10b598c0297ed51abf975e988f
sanjomo <git@santhoshmohan.com>
2025-11-27 18:50:56 +0530

    remove unused import

--- netty-socketio-core/src/main/java/com/socketio4j/socketio/protocol/JacksonJsonSupport.java
+++ netty-socketio-core/src/main/java/com/socketio4j/socketio/protocol/JacksonJsonSupport.java
@@ -22,7 +22,6 @@ import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
